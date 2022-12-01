d78a69454c0f4c9e34e5f3c09116bb20afd9f019
Jorge Muñoz <51250678+JorgeM-05@users.noreply.github.com>
2022-11-30 20:44:28 -0500

    v3 entity

--- src/main/java/com/promigasapp/apppromigas/entity/CountryEntity.java
+++ src/main/java/com/promigasapp/apppromigas/entity/CountryEntity.java
@@ -19,8 +19,32 @@ public class CountryEntity {
     private int pais;
 
     @JoinColumn(name = "fecha_actualizacion")
-    private String Greenfield;
+    private String fecha_actualizacion;
 
-    @JoinColumn(name = "MYA")
-    private String MYA;
+    @JoinColumn(name = "tipo_proyecto")
+    private String tipo_proyecto;
+
+    @JoinColumn(name = "ubicaion_geografica")
+    private String ubicaion_geografica;
+
+    @JoinColumn(name = "descripcion_oportunidad")
+    private String descripcion_oportunidad;
+
+    @JoinColumn(name = "seccion_supuestos")
+    private String seccion_supuestos;
+
+    @JoinColumn(name = "horizonte")
+    private String horizonte;
+
+    @JoinColumn(name = "POC")
+    private String POC;
+
+    @JoinColumn(name = "TRM")
+    private String TRM;
+
+    @JoinColumn(name = "activo_financiero")
+    private String activo_financiero;
+
+    @JoinColumn(name = "prioridad")
+    private String prioridad;
 }
